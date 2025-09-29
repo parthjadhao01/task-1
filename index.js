c19c852ebacb10b547676e49b184864301f2a02e
Parth Jadhao <parthjadhao@Parths-MacBook-Air.local>
2025-09-29 22:57:22 +0530

    exposing to access from any IP

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js server!");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
